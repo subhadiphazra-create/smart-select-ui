fa37d471dc3d51477e9ab55014330ff995d63ff1
subhadiphazra-create <subhadip.hazra@keross.com>
2025-08-31 22:43:51 +0530

    Create use.tsx

---
+++ use.tsx
@@ -0,0 +1,19 @@
+<SmartSelect
+                isMultiSelect
+                options={employees.map((e) => ({
+                  value: e.userId,
+                  label: `${e.basicData.firstName} ${e.basicData.lastName}`,
+                  role: e.basicData.role,
+                }))}
+                value={watch("batchTrainer")}
+                onChange={(val) =>
+                  setValue("batchTrainer", val as string[], {
+                    shouldValidate: true,
+                    shouldDirty: true,
+                  })
+                }
+                placeholder="Select trainers"
+                filterKey="role"
+                showFilter
+                showSearchbar
+              />
